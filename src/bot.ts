2ba734316e684f27797f9163edb174695e7c0b30
Aleksandr Sidorov <sidorovav@gmail.com>
2023-03-19 15:58:36 +0300

    translite

--- src/bot.ts
+++ src/bot.ts
@@ -167,12 +167,11 @@ bot.api.setMyCommands([
 ]);
 
 // Handle all other messages and the /start command
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
+const introductionMessage = `Привет. Я бот Александра Сидорова
 
 <b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]`;
+/yo - Выведет ваше имя
+/effect [text] - Некоторые преобразования над текстом [text]`;
 
 const replyWithIntro = (ctx: any) =>
   ctx.reply(introductionMessage, {
